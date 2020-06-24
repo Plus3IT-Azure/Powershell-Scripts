21704f78abc100d3a15a1320c9e13bfeb2f037ce
Kehinde.Owens <Kehinde.Owens@users.noreply.github.com>
2020-06-24 08:41:16 -0400

    -Added AZ CLI installation validation

--- az-appregistration-mg.ps1
+++ az-appregistration-mg.ps1
@@ -1,23 +1,48 @@
-﻿# This powershell script creates an App Registration for Cloudtamer.io and assigns it the Owner role to a Management Group
-# Command used to run script .\az-appregistration-mg.ps1 -ManagementGroupName <Management Group Name> -RedirectURL <Redirect URL>
+﻿# This powershell script creates an app registration for cloudtamer.io and assigns it the owner role to a management group
 
-# Input Variable(s)
-param($ManagementGroupName, $RedirectURL)
-Write-Host "The Management Group Name is $ManagementGroupName while the Redirect URL is $RedirectURL"
-Start-Sleep -s 5
+  # Command used to run script .\cloudtamer-appregistration.ps1 -ManagementGroupName <Management Group Name> -RedirectURL <Redirect URL> -CloudEnv <AzureCloud or AzureUSGovernment>
 
-# Log in to Azure (Have to authenticate with two cmdlets as some cmdlets are used with AZ Cli and some aren't)
+     # az version 2.8.0 must be installed in order for this powershell script to run successfully
+
+# Confirming AZ CLI is installed on localhost
+Write-Host "Verifying AZ CLI is installed..."
+ $azcli = az version --query '\"azure-cli\"'
+
+if($null -eq $azcli){
+  throw "Azure CLI not installed. Please install the Azure CLI and try again"
+    Write-Host "AZ CLI not installed; aborting script execution."
+     Exit    
+}
+else{
+  Write-Host "Azure CLI version $azcli is installed on localhost; moving forward with script execution"
+}
+Start-Sleep -s 3
+
+# Log in to Azure
 Write-Host "Authenticate into AzureAD"
 az login 
 $AzureContext = az account show --query '[tenantId,user.name]' -o tsv
-Connect-AzureAD -TenantId $AzureContext[0] -AccountId $AzureContext[1]
+
+# Set Azure Cloud Environment to Deploy Into (Commercial or Government)
+Write-Host "Setting Azure Environment to $CloudEnv"
+az cloud set --name $CloudEnv
 
 # Variables
 $AppRegName = "CT-MG-AppRegistration"
 $MSGraphId = "00000003-0000-0000-c000-000000000000"
-$domain = (Get-AzureADDomain).Name
 
-# API Permission IDs for Microsoft Graph
+# Get Commercial or Government Domain(s)
+if($CloudEnv -eq "AzureCloud"){
+  Write-Host "Retrieving Domain(s) for $CloudEnv Environment"
+$domain = az rest --uri https://graph.microsoft.com/v1.0/domains --query value[].id
+}
+if($CloudEnv -eq "AzureUSGovernment"){
+  Write-Host "Retrieving Domain(s) for $CloudEnv Environment"
+$domain = az rest --uri https://graph.microsoft.us/v1.0/domains --query value[].id
+}
+Start-Sleep -s 3
+
+# API Permission IDs for Microsoft Graph 
 $UserRead = "06da0dbc-49e2-44d2-8312-53f166ab848a=Scope"
 $DirectoryReadAll = "e1fe6dd8-ba31-4d61-89e7-88639da4683d=Scope"
 $UserReadAll = "62a82d76-70ea-41e2-9197-370581804d09=Role"
